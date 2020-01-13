59661c97d704cf00fd6291f2e4c7886c469f7b41
Hugo Peek <hugo@fractal-farming.com>
2020-01-13 14:07:35 +0800

    Add CSS and JS for full screen sliders

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     // COLLISION ALERT!!
     // There's a checkbox type that also uses the slider class, see issue #77 on Github.
-    $('.slider:not(.checkbox)')
+    // And in addition: there's also a Fomantic UI component now named Slider.
+    $('.slider-basic')
 
         // Initiate default slider
         .slick({
@@ -67,4 +68,33 @@ $(document).ready(function(){
             caption: 'caption'
         })
     ;
+
+    // Use Slick to create full-screen presentations
+    var fwh = $(window).height();
+    var fww = $(window).width();
+    var slider = $('.slider-fullscreen');
+
+    slider.find('.vertical.stripe.segment').attr('height',fwh);
+    slider.find('.vertical.stripe.segment').attr('width',fww);
+    slider.find('.slick-list').attr('height',fwh);
+    slider.slick({
+        adaptiveHeight: true,
+        variableWidth: true,
+        arrows: true,
+        dots: true,
+        centerMode: true
+    });
+});
+
+// Adjust dimensions of fullscreen slider on resize
+$(window).resize(function(){
+    var slider = $('.slider-fullscreen');
+    var i = slider.find('.vertical.stripe.segment');
+    var s = slider.find('.slick-list');
+
+    i.attr('width',$(window).width());
+    if(i.height() != $(window).height()){
+        i.attr('height',$(window).height());
+    }
+    s.height($(window).height());
 });
