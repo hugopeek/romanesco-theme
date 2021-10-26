c21f21ff66d777f5ecb83bf61f0aa7ee2704dd8e
Hugo Peek <hugo@fractal-farming.com>
2021-10-26 13:27:53 +0800

    Add Recaptcha polyfills for IE to FormBlocks JS

--- assets/js/formblocks.js
+++ assets/js/formblocks.js
@@ -153,3 +153,25 @@ $('.date.range.fields:not(.custom)').each(function() {
         startCalendar: $(this).find('.date.range.start')
     });
 });
+
+
+// Recaptcha
+// -----------------------------------
+
+// Polyfill for closest() and matches() for IE9+
+if (!Element.prototype.matches) {
+    Element.prototype.matches =
+        Element.prototype.msMatchesSelector ||
+        Element.prototype.webkitMatchesSelector
+    ;
+}
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        var el = this;
+        do {
+            if (Element.prototype.matches.call(el, s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
+}
